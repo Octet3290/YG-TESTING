87e23c9413a8f41825302a956818ae246d0c7e38
Rahul <rahul.yadav@yellowgap.com>
2025-03-07 15:23:07 +0530

    modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,29 +12,20 @@ AOS.init({
 
 });
 
-
-const nav = document.querySelector(".navigation");
-const root=document.documentElement;
-
-const endTransition = () => {
-const loader = document.querySelector(".loader");
-loader.addEventListener("transitionend", () => {
-  loader.style.transform = "translateX(100%)";
-  root.classList.remove("disable-hover");
+$(document).ready(function() {
+  $("#includeTransition").show();
+  $("#includeTransition").fadeIn(2000, function() {
+      setTimeout(function() {
+          $("#includeTransition").fadeOut(1500, function() {
+              $(this).hide();
+              $("body").css("overflow", "auto");  
+          });
+      }, 3000); 
+  });
 });
-loader.style.transform = "";
-};
 
-const startTransition = () => {
-const loader = document.querySelector(".loader");
-const navv = document.querySelector(".navbar");
-loader.style.transform = "translateX(100%)";
 
-};
 
-window.addEventListener("load", () => {
-startTransition();
-});
 
 
 
